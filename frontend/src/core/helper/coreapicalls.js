a9b720c9141608971359f3b9fa4d3e434bec4316
Bach Vu <pakvu@cs.uchicago.edu>
2021-01-26 00:43:49 +0700

    Update url to correctly get data

--- frontend/src/core/helper/coreapicalls.js
+++ frontend/src/core/helper/coreapicalls.js
@@ -1,10 +1,28 @@
 import { API } from "../../backend";
 
 export const getProducts = () => {
-  return fetch(`${API}product/`, { method: "GET" })
+  console.log(typeof API);
+  const url = "http://localhost:8000/api/v1/product/";
+  // return fetch(`${API}product/`, { method: "GET" })
+  //   .then((response) => {
+  //     return response.json();
+  //   })
+  //   .catch((err) => console.log(err));
+  return fetch(url, { method: "GET" })
     .then((response) => {
       return response.json();
     })
     .catch((err) => console.log(err));
 };
 // fetch returns a promise which you can use then and catch
+
+// import { API } from "../../backend";
+
+// export const getProducts = async () => {
+//   try {
+//     const response = await fetch(`${API}product`, { method: "GET" });
+//     return response.json();
+//   } catch (err) {
+//     return console.log(err);
+//   }
+// };
